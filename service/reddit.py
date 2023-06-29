7415a5d4edb7bcabbc39e26700811d50bbdae07d
Hayden Moritz <Moritz.Hayden@gmail.com>
2023-06-29 06:41:20 -0500

    Update reddit.py

--- service/reddit.py
+++ service/reddit.py
@@ -12,29 +12,22 @@ class RedditService:
         self.password: str | None = os.getenv('REDDIT_PASSWORD')
         self.user_agent: str = "MoritzHayden:reddit-drg-weekly-thread"
         self.check_for_async: bool = False
+        self.reddit = praw.Reddit(client_id=self.client_id,
+                                  client_secret=self.client_secret,
+                                  username=self.username,
+                                  password=self.password,
+                                  user_agent=self.user_agent,
+                                  check_for_async=self.check_for_async)
 
     def get_last_weekly_deep_dives_thread_url(self) -> str:
-        reddit = praw.Reddit(
-            client_id=self.client_id,
-            client_secret=self.client_secret,
-            user_agent=self.user_agent,
-            check_for_async=self.check_for_async
-        )
-        threads = reddit.subreddit("DeepRockGalactic").search(query="Weekly Deep Dives Thread",
-                                                              sort="hot",
-                                                              time_filter="week")
+        threads = self.reddit.subreddit("DeepRockGalactic")
+                             .search(query="Weekly Deep Dives Thread",
+                                     sort="hot",
+                                     time_filter="week")
         return next(threads).url
 
     def post_weekly_deep_dives_thread(self, thread_title: str, thread_text: str) -> None:
-        reddit = praw.Reddit(
-            client_id=self.client_id,
-            client_secret=self.client_secret,
-            username=self.username,
-            password=self.password,
-            user_agent=self.user_agent,
-            check_for_async=self.check_for_async
-        )
-        reddit.subreddit("DeepRockGalactic").submit(
+        self.reddit.subreddit("DeepRockGalactic").submit(
             title=thread_title,
             selftext=thread_text,
             flair_text="Discussion",
